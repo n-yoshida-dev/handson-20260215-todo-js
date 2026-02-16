a51b5e29dadcfa39c14cc283a0f4d558e7081fbe
n-yoshida-dev <annin.for.dev@gmail.com>
2026-02-16 13:58:13 +0900

    Added button tag generation function to JS.

--- src/main.js
+++ src/main.js
@@ -4,22 +4,37 @@ const onClickAdd = () => {
     // テキストボックスの値を取得→テキストボックスの値を初期化
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value="";
-    // alert(inputText);
 
     // li生成
     const li = document.createElement("li");
 
     // div生成→クラス名を追加
     const div = document.createElement("div");
-    div.className = "list-low";
+    div.className = "list-row";
 
     // pタグ生成→クラス名を追加→値を追加
     const p = document.createElement("p");
     p.className = "todo-item";
     p.innerText = inputText;
 
+    // button(完了)タグ生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+        alert("完了");
+    });
+
+    // button(削除)タグ生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除"
+    deleteButton.addEventListener("click", () => {
+        alert("削除");
+    });
+
     // liタグの子要素に各要素を設定(appendChild)
     div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
     li.appendChild(div);
 
     // 未完了リスト(ulタグ配下)にliを追加
