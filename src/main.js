86f3fbe45511ab2a13a4b6709ed59f5141b1fa17
n-yoshida-dev <annin.for.dev@gmail.com>
2026-02-16 13:14:57 +0900

    Add function(onClickAdd)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 import './style.css';
+
+const onClickAdd = () => {
+    // テキストボックスの値を取得→テキストボックスの値を初期化
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value="";
+    // alert(inputText);
+
+    // li生成
+    const li = document.createElement("li");
+
+    // div生成→クラス名を追加
+    const div = document.createElement("div");
+    div.className = "list-low";
+
+    // pタグ生成→クラス名を追加→値を追加
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = inputText;
+
+    // liタグの子要素に各要素を設定(appendChild)
+    div.appendChild(p);
+    li.appendChild(div);
+
+    // 未完了リスト(ulタグ配下)にliを追加
+    document.getElementById("incomplete-list").appendChild(li);
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
